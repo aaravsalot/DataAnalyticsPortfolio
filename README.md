b2b0f995419aac7602ef591389f864585a0b0cd0
aaravsalot <Aaravsalot@gmail.com>
2026-01-01 16:15:18 -0500

    Initialize dependabot/npm_and_yarn/ejs-3.1.10

---
+++ README.md
@@ -0,0 +1,78 @@
+# Portfolio Site Template 🚀
+
+[![forthebadge](https://forthebadge.com/images/badges/open-source.svg)](https://forthebadge.com)
+
+This portfolio site template can be used to showcase your past projects, career history, and skill sets. The example project is published with [GitHub Pages](https://yujisatojr.github.io/react-portfolio-template/).
+
+This template is free to use, and no attribution is required. You can fork or download this repository to customize it for your own use. Please don't forget to leave a ⭐ if you like this portfolio!
+
+![Demo](https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/portfolio-screenshot.png)
+
+## Features
+* Open source (no attribution required)
+* Multi-page layout
+* Responsive design
+* Simple & modern technologies (React, JavaScript, and SCSS)
+
+## Setup
+
+This application requires [Node.js](https://nodejs.org/). To check if you have Node.js installed, run:
+
+```bash
+node -v
+```
+
+In the project directory, run the following code to install dependencies:
+
+```bash
+npm install
+```
+
+Run the app in the development mode by using:
+
+```bash
+npm start
+```
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+Once you complete the above process, navigate to `/src/components` directory. Inside of it, you will find all the pages and components used within the template. You can then modify texts, pictures, and other information to your own.
+
+The page will reload if you make edits. You will also see any lint errors in the console.
+
+## Deployment
+
+You can choose your favorite service (e.g., [Netlify](https://www.netlify.com/), [Render](https://render.com/), [Heroku](https://www.heroku.com/)) for deployment. One of the easiest ways to host this portfolio is using GitHub pages. Follow the instructions below to complete the production deploy.
+
+1. Set Up GitHub Repository
+
+    Navigate to GitHub and create a new repository for the portfolio app.
+
+2. Configure `package.json`
+
+    In your `package.json` file, edit the following properties:
+
+    ```json
+    {
+        "homepage": "https://yourusername.github.io/your-repo-name",
+        "scripts": {
+            "predeploy": "npm run build",
+            "deploy": "gh-pages -d build",
+            ...
+        }
+    }
+    ```
+
+    Replace `yourusername` with your GitHub username and `your-repo-name` with the name of your GitHub repository.
+
+3. Deploy to GitHub Pages
+
+    Now you're ready to deploy your app. Run the following command:
+
+    ```bash
+    npm run deploy
+    ```
+
+4. Access Your Deployed App
+
+    After successfully deploying your app, you can access it at `https://yourusername.github.io/your-repo-name`.
---
+++ src/components/About.jsx
@@ -0,0 +1,90 @@
+import React, {useEffect} from "react";
+import '@fortawesome/free-regular-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons';
+import { VerticalTimeline, VerticalTimelineElement }  from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import '../assets/styles/About.scss'
+
+function About() {
+
+  useEffect(() => {
+    document.title = "Yuji Sato | About";
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+  }, []);
+
+  return (
+    <div>
+      <div className="header-image">
+        <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/about-image.jpg" alt="Asset by Oziel Gómez" />
+      </div>
+      <div className="items-container">
+        <div className="align-items-center">
+          <h1>Brief Summary</h1>
+          <p>
+            Yuji Sato is a Technology Consultant at Ernst & Young LLP, bringing consulting experience 
+            primarily in the Financial Services industry with a focus on credit card migration and payment 
+            domain knowledge. He began his career in a software engineering role, providing business solutions 
+            through leveraging emerging technology. His expertise includes full stack web development, DevOps, 
+            automation, and project management.
+          </p>
+        </div>
+        <h1>Career Timeline</h1>
+        <VerticalTimeline>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            contentStyle={{ background: 'white', color: 'rgb(39, 40, 34)' }}
+            contentArrowStyle={{ borderRight: '7px solid  white' }}
+            date="2022 - present"
+            iconStyle={{ background: '#FFD300', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Technology Consultant</h3>
+            <h4 className="vertical-timeline-element-subtitle">Dallas, TX</h4>
+            <p>
+              Full-stack Web Development, GenAI/LLM, Project Management, Business Development
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2020 - 2022"
+            iconStyle={{ background: '#FFD300', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Full Stack Engineer</h3>
+            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <p>
+              Frontend Development, Backend Development, User Experience, Team Leading
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2021 - 2021"
+            iconStyle={{ background: '#FFD300', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Staff Engineer Intern</h3>
+            <h4 className="vertical-timeline-element-subtitle">Laie, HI</h4>
+            <p>
+              Full-stack Development, API Development, User Experience
+            </p>
+          </VerticalTimelineElement>
+          <VerticalTimelineElement
+            className="vertical-timeline-element--work"
+            date="2020 - 2020"
+            iconStyle={{ background: '#FFD300', color: 'rgb(39, 40, 34)' }}
+            icon={<FontAwesomeIcon icon={faBriefcase} />}
+          >
+            <h3 className="vertical-timeline-element-title">Data Analyst Intern</h3>
+            <h4 className="vertical-timeline-element-subtitle">Tokyo, Japan</h4>
+            <p>
+              Automation, Data Governance, Statistical Analysis
+            </p>
+          </VerticalTimelineElement>
+        </VerticalTimeline>
+      </div>
+    </div>
+  );
+}
+
+export default About;
